757c84fc1d07b7813adc6a807878c071c7bdac4b
Daniele Parisi <daniele@parisi.xyz>
2018-08-12 14:56:08 +0200

    sequence validity

--- ch5/tables.lua
+++ ch5/tables.lua
@@ -18,6 +18,26 @@ function polyval2(t, x)
   return res
 end
 
+-- Ex 5.6
+-- Write a function to test whether a given table is a valid sequence.
+-- (I consider only holes in keys with positive integer values)
+function isseq(t)
+  max = 0
+  for i in ipairs(t) do
+    max = i
+  end
+  for k in pairs(t) do
+    if type(k) == "number" and k > max and math.floor(k) == k then
+      return false
+    end
+  end
+  return true
+end
+
+assert(isseq({3,4,5,"lua"}))
+assert(isseq({3,4,5, n = 4,"lua"}))
+assert(not isseq({3,4,5, "lua", [6] = 6}))
+
 -- Ex 5.7 Write a function that inserts all elements of a given list into a
 -- given position of another given list.
 function insertallat(t1, p, t2)
