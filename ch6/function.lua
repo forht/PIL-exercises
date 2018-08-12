607c6021d872e12a68bcc7807f56d84a22a81b83
Daniele Parisi <daniele@parisi.xyz>
2018-08-12 18:14:35 +0200

    shuffle

--- ch6/function.lua
+++ ch6/function.lua
@@ -13,3 +13,22 @@ function exceptlast(...)
   table.remove(args, args.n)
   return table.unpack(args)
 end
+
+-- Ex 6.4 Write a function to shuffle a given list. Make sure that all
+-- permutations are equally probable.
+function swap(t, i, j)
+  local temp = t[i]
+  t[i] = t[j]
+  t[j] = temp
+end
+-- Fisher-Yates shuffle
+function shuffle(t)
+  math.randomseed(os.time())
+  for i = #t, 2, -1 do
+    swap(t, i, math.random(i))
+  end
+end
+
+local t = {1,2,3,4,5,6,7,8,9}
+shuffle(t)
+print(table.unpack(t))
