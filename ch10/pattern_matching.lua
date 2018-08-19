9c583463ddb31c71cc4724017298c32c6583b20b
Daniele Parisi <daniele@parisi.xyz>
2018-08-19 09:34:41 +0200

    transliterate

--- ch10/pattern_matching.lua
+++ ch10/pattern_matching.lua
@@ -21,6 +21,16 @@ function split(s, d)
   return t
 end
 
+-- Ex 10.3: Write a function transliterate. This function receives a string and
+-- replaces each character in that string with another character, according to a
+-- table given as a second argument. If the table maps a to b, the function
+-- should replace any occurrence of a with b. If the table maps a to false, the
+-- function should remove occurrences of a from the resulting string.
+
 function transliterate(s, t)
-  string.gsub(s, ".", function(c) return t[c] or "" end)
+  local function tr(c)
+    local m = t[c]
+    return m == false and "" or m
+  end
+  return string.gsub(s, ".", tr)
 end
