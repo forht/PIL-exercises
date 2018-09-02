6bf465af199c79db79a0d527f8b3b84650a9a378
Daniele Parisi <daniele@parisi.xyz>
2018-09-02 20:56:11 +0200

    forgot header

--- ch14/nodelabel.lua
+++ ch14/nodelabel.lua
@@ -1,3 +1,12 @@
+-- Exercise 14.3: Modify the graph structure so that it can keep a label for
+-- each arc. The structure should represent each arc by an object, too, with two
+-- fields: its label and the node it points to. Instead of an adjacent set, each
+-- node keeps an incident set that contains the arcs that originate at that
+-- node.
+--
+-- Adapt the function readgraph to read two node names plus a label from each
+-- line in the input file. (Assume that the label is a number.)
+
 local function name2node (graph, name)
   local node = graph[name]
   if not node then
